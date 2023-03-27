f29d735a49f95985f82329dc357829a72631dc8d
oio972 <christophe.jp77@gmail.com>
2023-03-27 14:11:24 +0200

    projet 5

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,28 +2,37 @@ const slides = [
   {
     image: "slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-    //i: "dot dot_selected fa-regular fa-circle",
   },
   {
     image: "slide2.jpg",
     tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-    //i: "dot fa-regular fa-circle",
   },
   {
     image: "slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-    //i: "dot fa-regular fa-circle",
   },
   {
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-    //i: "dot fa-regular fa-circle",
   },
 ];
 
-//methode 1
 let compteur = 0;
 let point = 0;
+
+let changementsource = function() {
+	document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
+  document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
+
+  if(document.querySelector(".dot_selected")!= null){
+    document.querySelector(".dot_selected").classList.remove("dot_selected");
+  }
+
+  if(document.getElementById("point"+compteur)!= null){
+    document.getElementById("point"+compteur).classList.add("dot_selected");
+  }
+
+}
 
 let arrow_left = document.getElementById("arrow_left");
 arrow_left.addEventListener("click", () => {
@@ -32,18 +41,7 @@ arrow_left.addEventListener("click", () => {
 	if (compteur < 0) {
 		compteur = slides.length -1;
 	}
-	document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
-  document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
-  document.getElementById("point").hasAttribute = slides[compteur].tagLine;
-});
-arrow_left.addEventListener("click", () => {
-  point--;
-  if (point < 0) {
-    point = slides.length -1;
-  }
-  document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
-  document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
-  //document.getElementById("point")
+  changementsource();
 });
 
 let arrow_right = document.getElementById("arrow_right");
@@ -53,41 +51,23 @@ arrow_right.addEventListener("click", () => {
 	if (compteur >= slides.length) {
 		compteur = 0;
 	}
-	document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
-	document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
-   //document.getElementById("point")
+  changementsource();
 });
-arrow_right.addEventListener("click", () => {
-    
-});
+// traitement du click sur les dots
+let elements = document.getElementsByClassName("dot");
+
+let myFunction = function() {
+    let attribute = this.attribute("id");
+    compteur = attribute.replace("point","");
+    changementsource();
+  };
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', myFunction, false);
+}
 
 
 
-/*
-//methode 2
-arrow_left.addEventListener("click", () => {
-  //alert("fleche de gauche");
-  compteur--;
-  point--;
-  if (compteur < 0, point < 0) {
-    compteur = slides.length -1
-    point = slides.length -1;
-  }
-  document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
-  document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
-  //document.getElementById("point")
-});
+   
 
-arrow_right.addEventListener("click", () => {
-  //alert("fleche de droite");
-  compteur++;
-  point++;
-  if (compteur >= 0, point >= 0) {
-    compteur = slides.length +1
-    point = slides.length +1
-  }
-  document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
-  document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
- //document.getElementById("point")
-});
-*/
+
