3a73642a208e0a9a332bbea0c6dc83a409bee40b
oio972 <christophe.jp77@gmail.com>
2023-03-28 09:23:25 +0200

    projet 5 terminé

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+//tableau images
 const slides = [
   {
     image: "slide1.jpg",
@@ -16,10 +17,10 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-
+//compteur
 let compteur = 0;
 let point = 0;
-
+//function
 let changementsource = function() {
 	document.getElementById("image1").src = "assets/images/slideshow/" + slides[compteur].image;
   document.getElementById("tag1").innerHTML = slides[compteur].tagLine;
@@ -31,9 +32,19 @@ let changementsource = function() {
   if(document.getElementById("point"+compteur)!= null){
     document.getElementById("point"+compteur).classList.add("dot_selected");
   }
-
 }
-
+// traitement du click sur les dots
+let elements = document.getElementsByClassName("dot");
+let myFunction = function() {
+    let attribute = this.attribute("id");
+    compteur = attribute.replace("point","");
+    changementsource();
+  };
+//boucle
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', myFunction, false);
+}
+//fleche de gauche
 let arrow_left = document.getElementById("arrow_left");
 arrow_left.addEventListener("click", () => {
   //alert("fleche de gauche");
@@ -43,7 +54,7 @@ arrow_left.addEventListener("click", () => {
 	}
   changementsource();
 });
-
+//fleche de droite
 let arrow_right = document.getElementById("arrow_right");
 arrow_right.addEventListener("click", () => {  
   //alert("fleche de droite");
@@ -53,18 +64,7 @@ arrow_right.addEventListener("click", () => {
 	}
   changementsource();
 });
-// traitement du click sur les dots
-let elements = document.getElementsByClassName("dot");
 
-let myFunction = function() {
-    let attribute = this.attribute("id");
-    compteur = attribute.replace("point","");
-    changementsource();
-  };
-
-for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', myFunction, false);
-}
 
 
 
